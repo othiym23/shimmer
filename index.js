a577f2742a27b58dd48c2177990d7d78c33d64c0
Forrest L Norvell <forrest@newrelic.com>
2013-10-27 17:14:22 -0700

    code: add wrapEmitter for generic EE extension
    Mostly useful in CLS, but generic enough to be applicable elsewhere.

--- index.js
+++ index.js
@@ -83,8 +83,98 @@ function unwrap(nodule, name) {
   }
 }
 
+function wrapEmitter(emitter, mark, prepare) {
+  if (!emitter || !emitter.on || !emitter.addListener || !emitter.emit) {
+    logger("can only bind real EEs");
+    return;
+  }
+
+  if (!mark) {
+    logger("must pass marker function to wrap emitter");
+    return;
+  }
+
+  if (!prepare) {
+    logger("must pass preparation function to wrap emitter");
+    return;
+  }
+
+  /* Attach a context to a listener, and make sure that this hook stays
+   * attached to the emitter forevermore.
+   */
+  function capturer(on) {
+    return function captured(event, listener) {
+      // set up the listener so that prepare can do whatever it needs
+      mark(listener);
+
+      try {
+        return on.call(this, event, listener);
+      }
+      finally {
+        // old-style streaming overwrites .on and .addListener, so rewrap
+        if (!this.on.__wrapped) wrap(this, 'on', capturer);
+        if (!this.addListener.__wrapped) wrap(this, 'addListener', capturer);
+      }
+    };
+  }
+
+  function puncher(emit) {
+    return function punched(event) {
+      if (!this._events || !this._events[event]) return emit.apply(this, arguments);
+
+      var unwrapped = this._events[event];
+
+      /* Ensure that if removeListener gets called, it's working with the
+       * unwrapped listeners.
+       */
+      function releaser(removeListener) {
+        return function unwrapRemove() {
+          this._events[event] = unwrapped;
+          try {
+            return removeListener.apply(this, arguments);
+          }
+          finally {
+            unwrapped = this._events[event];
+            this._events[event] = prepare(unwrapped);
+          }
+        };
+      }
+      wrap(this, 'removeListener', releaser);
+
+      try {
+        /* At emit time, ensure that whatever else is going on, removeListener will
+         * still work while at the same time running whatever hooks are necessary to
+         * make sure the listener is run in the correct context.
+         */
+        this._events[event] = prepare(unwrapped);
+        return emit.apply(this, arguments);
+      }
+      finally {
+        /* Ensure that regardless of what happens when preparing and running the
+         * listeners, the status quo ante is restored before continuing.
+         */
+        unwrap(this, 'removeListener');
+        this._events[event] = unwrapped;
+      }
+    };
+  }
+
+  wrap(emitter, 'addListener', capturer);
+  wrap(emitter, 'on',          capturer);
+  wrap(emitter, 'emit',        puncher);
+
+  emitter.__unwrap = function () {
+    if (emitter.addListener.__unwrap) emitter.addListener.__unwrap();
+    if (emitter.on.__unwrap) emitter.on.__unwrap();
+    if (emitter.emit.__unwrap) emitter.emit.__unwrap();
+    delete emitter.__wrapped;
+  };
+  emitter.__wrapped = true;
+}
+
 shimmer.wrap = wrap;
 shimmer.massWrap = massWrap;
 shimmer.unwrap = unwrap;
+shimmer.wrapEmitter = wrapEmitter;
 
 module.exports = shimmer;
