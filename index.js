a9b6abfec16fed9968e6b6b0ab3ad38191aa4b17
Stephen Belanger <admin@stephenbelanger.com>
2016-04-21 14:55:12 -0700

    Support generator functions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 function isFunction (funktion) {
-  return funktion && {}.toString.call(funktion) === '[object Function]'
+  return funktion && /\[object (Generator)?Function\]/.test({}.toString.call(funktion))
 }
 
 // Default to complaining loudly when things don't go according to plan.
